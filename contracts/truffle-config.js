970d6e24990970514c1f4713e72453b31161c2e6
Your Name <you@example.com>
2023-03-18 16:03:22 +0530

    new

---
+++ contracts/truffle-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    advanced: {
+      websocket: true,
+    },
+  },
+  contract_build_directory: "./src/abis",
+  compilers: {
+    solc: {
+	version: "0.8.19",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
+
+
